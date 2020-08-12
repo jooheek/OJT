7e5b6286a3374aa8bd8af0d6ad7c9730823e69ea
jooheek <kimjoohee713@gmail.com>
2020-08-12 14:35:43 +0900

    Create pagination

---
+++ test/src/main/java/org/joohee/common/Pagination.java
@@ -0,0 +1,43 @@
+/**
+ * <PRE>
+ *  Project : test
+ *  Package : org.joohee.common
+ * </PRE>
+ * @file   : Pagination.java
+ * @date   : 2020. 8. 12 오전 10:28:26
+ * @author : NB-0308
+ * @brief  :
+ *  변경이력    : 
+ *        이름     : 일자          : 근거자료   : 변경내용
+ *       ------------------------------------
+ *        NB-0308  : 2020. 8. 12       :            : 신규 개발.
+ */
+package org.joohee.common;
+
+/**
+ * <PRE>
+ *  ClassName : Pagination 
+ * </PRE>
+ * @version : 1.0
+ * @date    : 2020. 8. 12 오전 10:28:26
+ * @author  : NB-0308
+ * @brief   : 
+ */
+
+public class Pagination {
+	
+	private int listSize=10;
+	private int rangeSixe=10;
+	private int page;
+	private int range;
+	private int listCnt;
+	private int pageCnt;
+	private int startPage;
+	private int startList;
+	private int endPage;
+	private boolean prev;
+	private boolean next;
+	
+	
+
+}
